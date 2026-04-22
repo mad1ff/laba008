24d56ff756c836db2f68e1dc1380e327dff3a9c5
mad1ff <madinasaidova426@gmail.com>
2026-04-23 00:58:12 +0500

    Update 00_distance.py

--- lab01(2)/00_distance.py
+++ lab01(2)/00_distance.py
@@ -15,6 +15,14 @@ sites = {
 distances = {}
 
 # TODO здесь заполнение словаря
+for city1, coords1 in sites.items():
+    distances[city1] = {}
+    for city2, coords2 in sites.items():
+        if city1 != city2:
+            x1, y1 = coords1
+            x2, y2 = coords2
+            dist = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+            distances[city1][city2] = dist
 
 print(distances)
 
