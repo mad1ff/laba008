185df278d313edc86e006fc995d19e65dc5bff6c
mad1ff <madinasaidova426@gmail.com>
2026-04-30 00:45:39 +0500

    00_distance.py

--- lab01(2)/00_distance.py
+++ lab01(2)/00_distance.py
@@ -1,30 +1,33 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-# Есть словарь координат городов
-
 sites = {
     'Moscow': (550, 370),
     'London': (510, 510),
     'Paris': (480, 480),
 }
 
-# Составим словарь словарей расстояний между ними
-# расстояние на координатной сетке - ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+def calculate_distances(cities):
+        distances = {}
+        
+        for city1, coords1 in cities.items():
+            city_distances = {}
+            for city2, coords2 in cities.items():
+                if city1 != city2:
+                    x1, y1 = coords1
+                    x2, y2 = coords2
+                    distance = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+                    city_distances[city2] = round(distance, 2)
+            
+            distances[city1] = city_distances
+        
+        return distances
 
-distances = {}
 
-# TODO здесь заполнение словаря
-for city1, coords1 in sites.items():
-    distances[city1] = {}
-    for city2, coords2 in sites.items():
-        if city1 != city2:
-            x1, y1 = coords1
-            x2, y2 = coords2
-            dist = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
-            distances[city1][city2] = dist
+def goroda():
+    distances = calculate_distances(sites)
+    print(distances)
 
-print(distances)
+
+
+
 
 
 
